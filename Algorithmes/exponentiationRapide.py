70f2a9e34b0678dcfdc4b6b96266dabf0833c257
FlavioMrrn <flavio.mrrn@gmail.com>
2022-11-22 10:14:51 +0100

    Ajout exponentiation rapoide

---
+++ Algorithmes/exponentiationRapide.py
@@ -0,0 +1,17 @@
+def exponentiationRapide(a,x,n):
+    r = 1
+    e = x
+    b = a % n
+    while e > 0:
+        y = e % 2
+        r = (r * pow(b,y) % n)
+        b = (b*b)%n
+        e = e//2
+    return [r, b, e]
+
+
+def main():
+    print(exponentiationRapide(6, 55, 23))
+
+if __name__ == "__main__":
+    main()
