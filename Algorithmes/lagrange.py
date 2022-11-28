dcb73eb06e0317f51dbc1cc935f554a451fad027
FlavioMrrn <flavio.mrrn@gmail.com>
2022-11-28 09:12:05 +0100

    evaluation de polynomes

--- Algorithmes/lagrange.py
+++ Algorithmes/lagrange.py
@@ -1,5 +1,16 @@
+def lagrange(points):
+    resultats = ""
+        
+
+def evaluationPolynome(polynome, x):
+    resultat = 0
+    for idx, v in enumerate(polynome):
+        resultat += v * pow(x,idx)
+    return resultat
+
+
 def main():
-    print("Hello world!")
+    print(evaluationPolynome([1,2,3], 4))
 
 if __name__ == "__main__":
     main()
