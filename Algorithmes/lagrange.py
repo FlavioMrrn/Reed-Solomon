c763e41ecb97d6878bb353e366667b03a785d16b
FlavioMrrn <flavio.mrrn@gmail.com>
2022-12-05 08:46:54 +0100

    modification des fonctions profs en modulo

--- Algorithmes/lagrange.py
+++ Algorithmes/lagrange.py
@@ -4,19 +4,21 @@ def lagrange(p):
         produit = [1]
         for j in range(len(p)) :
             if i!= j :
-                produit = multiplication(produit, [1/(p[i][0] - p[j][0]), -(p[j][0]/(p[i][0] - p[j][0]))])
+                produit = multiplication(produit, [1/(p[i][0] - p[j][0]), -(p[j][0]/(p[i][0] - p[j][0]))])#(x-xk)/(xi-xl) soit: [1/(xi-xk)]*x + (-xk)/(xi-xk)
         produit = multiplication(produit, [p[i][1]])
         somme = addition(somme, produit)
-    return reversed(somme)
+    return reversed(somme)#simplifie le polynome
 
-def addition(a,b):
+def addition(a,b,n):
     m = max(len(a),len(b))
     s0 = [0 for k in range(m)]
     for k in range(m):
         if (k<len(a)):
             s0[k] += a[k]
+            s0[k] %= n
         if (k<len(b)):
             s0[k] += b[k]
+            s0[k] %= n
     return remove_zeros(s0)
 
 
@@ -32,6 +34,7 @@ def multiplication(a,b):
     for k in range(len(a)):
        for l in range(len(b)):
             prod[k+l] +=  a[k]*b[l] 
+            prod[k+l] %= n
     return prod
             
 def evaluationPolynome(polynome, x):
