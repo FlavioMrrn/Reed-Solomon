4308fb90ca379d08dea3c51623c7631a405138b2
PasJad <jtnproj@gmail.com>
2022-12-05 08:50:05 +0100

    feat: inverse_mod

--- Algorithmes/lagrange.py
+++ Algorithmes/lagrange.py
@@ -9,6 +9,59 @@ def lagrange(p):
         somme = addition(somme, produit)
     return reversed(somme)
 
+def pgcd_bezout(a, b):
+    r0 = a
+    r1 = b
+    x0 = 1
+    y0 = 0
+    x1 = 0
+    y1 = 1
+    x = 0
+    y = 0
+    r = 1
+    while(r != 0):
+        q = r0 // r1
+        r = r0 - q * r1
+        x = x0 - q * x1
+        y = y0 - q * y1
+        x0 = x1
+        x1 = x
+        y0 = y1
+        y1 = y
+        r0 = r1
+        r1 = r
+    return [r0, x0, y0]
+
+def pgcd_bezout(a, b):
+    r0 = a
+    r1 = b
+    x0 = 1
+    y0 = 0
+    x1 = 0
+    y1 = 1
+    x = 0
+    y = 0
+    r = 1
+    while(r != 0):
+        q = r0 // r1
+        r = r0 - q * r1
+        x = x0 - q * x1
+        y = y0 - q * y1
+        x0 = x1
+        x1 = x
+        y0 = y1
+        y1 = y
+        r0 = r1
+        r1 = r
+    return [r0, x0, y0]
+
+def mod_inverse(a,b):
+    coeffs = pgcd_bezout(a,b)
+    return coeffs[1]
+
+    
+
+
 def addition(a,b):
     m = max(len(a),len(b))
     s0 = [0 for k in range(m)]
